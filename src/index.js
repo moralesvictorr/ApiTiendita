42d33c30d3a96de463db83740d3de8a834542e63
moralesvictorr <vmmorales@unicauca.edu.co>
2023-01-26 17:54:46 -0500

    feat: general commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('${__dirname}/data/chaside.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use((req,res,next)=>{
+
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","*");
+    next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port,()=>{
+    console.log(`JSON Server is running on port ${port}`);
+})
