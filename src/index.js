9a59b3960487cb3364571a9e19be027b88dfc460
moralesvictorr <vmmorales@unicauca.edu.co>
2023-01-26 18:13:40 -0500

    feat: router changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('${__dirname}/data/chaside.json');
+const router = jsonServer.router('./data/chaside.json');
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
